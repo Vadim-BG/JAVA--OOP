cbdc137d180d713a01151fbe69173c6ce864bb9b
Vadim-BG <infotourbg.vadim@gmail.com>
2019-07-30 12:04:51 +0300

    added Dog

--- src/com/javalesson/oop/Dog.java
+++ src/com/javalesson/oop/Dog.java
@@ -2,13 +2,39 @@ package com.javalesson.oop;
 
 public class Dog {
 
-    private int paws;
-    private int tail;
-    private String name;
-    private String breed;
+    private static int dogsCount;
+
+    private int paws;//кол-во лап
+    private int tail;//кол-во хвостов
+    private String name;//Имя
+    private String breed;//Порода
+    private String size;//Размер собаки
+
+    public Dog() {//конструктор
+        dogsCount++;
+        System.out.println("Dog's count is "+dogsCount);//общее количество собак
+    }
+
+    public static int getDogsCount() {
+        return dogsCount;
+    }
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        if (size.equalsIgnoreCase("Big") ||
+                size.equalsIgnoreCase("Average") ||
+                size.equalsIgnoreCase("Small")) {
+            this.size = size;
+        } else {
+            System.out.println("Size should be one of these: Big, Average or Small");//Размер должен быть одним из трех
+        }
     }
 
     public String getName() {
@@ -50,5 +76,23 @@ public class Dog {
 
     public void setBreed(String breed) {
         this.breed = breed;
+    }
+
+    public void bark() {  //Лает
+        if (size.equalsIgnoreCase("Big")) {
+            System.out.println("Wof-Wof");//Если большая
+        } else if (size.equalsIgnoreCase("Average")) {
+            System.out.println("Raf-Raf");//Если средняя
+        } else {
+            System.out.println("Tiaf-tiaf");//Если маленькая
+        }
+    }
+
+    public void bite() { //Кусает
+        if (dogsCount > 2) {//Если собак больше двух
+            System.out.println("Dog's are biting you");// Собаки кусают вас
+        } else {
+            bark();//в противном случае, лают
+        }
     }
 }
--- src/com/javalesson/oop/Main.java
+++ src/com/javalesson/oop/Main.java
@@ -2,11 +2,17 @@ package com.javalesson.oop;
 
 public class Main {
     public static void main(String[] args) {
+
+        System.out.println("Dog's count "+Dog.getDogsCount());
+
         Dog lab = new Dog();//Лабрадор
-        lab.setPaws(5);//количество лап
-        lab.setTail(2);//количество хвостов
+        lab.setPaws(4);//количество лап
+        lab.setTail(1);//количество хвостов
         lab.setName("Charley");//имя
         lab.setBreed("Lab");//порода
+        lab.setSize("Average");//размер
+        //lab.bark();
+        lab.bite();
 
 
         Dog sheppard = new Dog();//Овчарка
@@ -14,10 +20,23 @@ public class Main {
         sheppard.setTail(1);//количество хвостов
         sheppard.setName("Mike");//имя
         sheppard.setBreed("Sheppard");//порода
+        sheppard.setSize("Big");//размер
+        //sheppard.bark();
+        sheppard.bite();
 
-        System.out.println("Lab's name is "+lab.getName());
+        Dog doberman = new Dog();
+        doberman.setPaws(4);//количество лап
+        doberman.setTail(1);//количество хвостов
+        doberman.setName("Jack");//имя
+        doberman.setBreed("Doberman");//порода
+        doberman.setSize("Small");//размер
+        //doberman.bark();
+        doberman.bite();
+
+
+        /*System.out.println("Lab's name is "+lab.getName());
         System.out.println("Sheppard's name is "+sheppard.getName());
-        System.out.println("Lab has "+lab.getPaws()+" paws");
+        System.out.println("Lab has "+lab.getPaws()+" paws");*/
 
 
 
