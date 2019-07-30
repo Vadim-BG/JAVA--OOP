29f413446937f135090a7bd5e4317e34f7d6b612
Vadim-BG <infotourbg.vadim@gmail.com>
2019-07-30 10:13:29 +0300

    added comments

--- src/com/javalesson/oop/Main.java
+++ src/com/javalesson/oop/Main.java
@@ -2,18 +2,18 @@ package com.javalesson.oop;
 
 public class Main {
     public static void main(String[] args) {
-        Dog lab = new Dog();
-        lab.setPaws(5);
-        lab.setTail(2);
-        lab.setName("Charley");
-        lab.setBreed("Lab");
+        Dog lab = new Dog();//Лабрадор
+        lab.setPaws(5);//количество лап
+        lab.setTail(2);//количество хвостов
+        lab.setName("Charley");//имя
+        lab.setBreed("Lab");//порода
 
 
-        Dog sheppard = new Dog();
-        sheppard.setPaws(4);
-        sheppard.setTail(1);
-        sheppard.setName("Mike");
-        sheppard.setBreed("Sheppard");
+        Dog sheppard = new Dog();//Овчарка
+        sheppard.setPaws(4);//количество лап
+        sheppard.setTail(1);//количество хвостов
+        sheppard.setName("Mike");//имя
+        sheppard.setBreed("Sheppard");//порода
 
         System.out.println("Lab's name is "+lab.getName());
         System.out.println("Sheppard's name is "+sheppard.getName());
