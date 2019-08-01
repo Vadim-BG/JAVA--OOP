ac360ba26d784b2d9bc178dc7a86496d354e522d
Vadim-BG <infotourbg.vadim@gmail.com>
2019-08-01 13:39:23 +0300

    +Constructors

---
+++ src/com/javalesson/constructors/ConstructorsMain.java
@@ -0,0 +1,13 @@
+package com.javalesson.constructors;
+
+import com.javalesson.domainmodel.Employee;
+
+
+public class ConstructorsMain {
+    public static void main(String[] args) {
+
+        Employee employee = new Employee("Alex","developer",100);
+        System.out.println(employee);
+
+    }
+}
