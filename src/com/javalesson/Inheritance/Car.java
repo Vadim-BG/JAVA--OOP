68287d2a0bad1806adf774bb7be94e1341f5efad
Vadim-BG <infotourbg.vadim@gmail.com>
2019-08-05 12:05:47 +0300

    Inheritance

---
+++ src/com/javalesson/Inheritance/Car.java
@@ -0,0 +1,9 @@
+package com.javalesson.Inheritance;
+
+public class Car extends Auto {
+
+
+    public Car(String producer, String model, EngineType engineType) {
+        super(producer, model, engineType);
+    }
+}
---
+++ src/com/javalesson/Inheritance/Truck.java
@@ -0,0 +1,8 @@
+package com.javalesson.Inheritance;
+
+public class Truck extends Auto {
+
+    public Truck(String producer, String model, EngineType engineType) {
+        super(producer, model, engineType);
+    }
+}
