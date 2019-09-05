e7de8a7725899016c7c76a1b0d316ed2801c867f
Vadim-BG <infotourbg.vadim@gmail.com>
2019-09-05 10:35:21 +0300

    Exception Handling Part 2

--- src/com/javalesson/exceptions/ExceptionHandlingMain.java
+++ src/com/javalesson/exceptions/ExceptionHandlingMain.java
@@ -1,37 +1,46 @@
 package com.javalesson.exceptions;
 
-import java.util.InputMismatchException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Scanner;
 
 public class ExceptionHandlingMain {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         Scanner scanner = new Scanner(System.in);
         boolean continueLoop = true;
+        PrintWriter writer = null;
         do {
             try {
                 System.out.println("Please enter numerator");
                 int numerator = scanner.nextInt();
                 System.out.println("Please enter denominator");
                 int denominator = scanner.nextInt();
-                System.out.println(divide(numerator, denominator));
+//                System.out.println(divide(numerator, denominator));
+                writer = new PrintWriter(new FileWriter("out.txt"));
+                writer.println("Result = " + divide(numerator, denominator));
                 continueLoop = false;
-            } catch (ArithmeticException e) {
-                System.out.println("Exception : " + e);
-                scanner.nextLine();
-                System.out.println("Only non-zero parameters allowed");
-            } catch (InputMismatchException e) {
-                System.out.println("Exception : " + e);
-                scanner.nextLine();
-                System.out.println("Only integer values allowed");
+            }
+//            catch (ArithmeticException | InputMismatchException e) {
+//                System.out.println("Exception : " + e);
+//                scanner.nextLine();
+//                System.out.println("Only integer non-zero parameters allowed");
+//            } catch (IOException e) {
+//                System.out.println("Unable to open file");
+//                e.printStackTrace();
+//            }
+            finally {
+                System.out.println("Finally block called");
+                writer.close();
             }
             System.out.println("Try catch block finished");
-        }while (continueLoop);
+        } while (continueLoop);
 
     }
 
-    private static int divide(int numerator, int denominator) {
+    private static int divide(int numerator, int denominator) throws ArithmeticException, NullPointerException {
         return numerator / denominator;
     }
 
