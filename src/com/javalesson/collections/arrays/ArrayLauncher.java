90e7ca8220366dae14bc81a76422e8d2b0296dc0
Vadim-BG <infotourbg.vadim@gmail.com>
2019-09-22 10:57:05 +0300

    Array Part 1

--- src/com/javalesson/collections/arrays/ArrayLauncher.java
+++ src/com/javalesson/collections/arrays/ArrayLauncher.java
@@ -9,15 +9,30 @@ public class ArrayLauncher {
     public static void main(String[] args) {
         int[] myArray = new int[8];
         System.out.println("Please enter 8 int elements");
-        for (int j = 0; j <myArray.length ; j++) {
+        for (int j = 0; j < myArray.length; j++) {
             System.out.println("Next element");
             myArray[j] = scanner.nextInt();
         }
-
-
-        for (int i = 0; i < myArray.length; i++) {
+        int[] array = sort(myArray);
+        for (int i = 0; i < array.length; i++) {
             System.out.println("Element #" + i + " = " + myArray[i]);
         }
 
     }
+
+    private static int[] sort(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+
+        }
+        return array;
+    }
+
+    // 10, 5, 16, 4
 }
