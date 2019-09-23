4461c1cffd2f76e54c787f8bf61dd505226a709c
Vadim-BG <infotourbg.vadim@gmail.com>
2019-09-23 11:21:45 +0300

    Array Part 4

--- src/com/javalesson/collections/arrays/dies.java
+++ src/com/javalesson/collections/arrays/dies.java
@@ -10,9 +10,11 @@ public class dies {
             ++frequency[1 + random.nextInt(6)];
         }
 
-//        for (int i = 1; i < frequency.length; i++) {
-//            System.out.println("Side " + i + ": " + frequency[i]);
-//        }
+        for (int i = 1; i < frequency.length; i++) {
+            System.out.println("Side " + i + ": " + frequency[i]);
+        }
+
+
         multidimArrayLauncher();
     }
 
