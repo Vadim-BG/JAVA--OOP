8d022e0aee072cd68282b80887250435f8636193
Vadim-BG <infotourbg.vadim@gmail.com>
2019-10-16 12:42:56 +0300

    Java HashMap part 1

--- src/com/javalesson/collections/map/MapLauncher.java
+++ src/com/javalesson/collections/map/MapLauncher.java
@@ -1,17 +1,46 @@
 package com.javalesson.collections.map;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class MapLauncher {
 
     public static void main(String[] args) {
-        Map<String, Integer> numbers = new HashMap<>();
-        numbers.put("Toyota", 50);
-        numbers.put("Audi", 51);
-        System.out.println(numbers.put("Toyota", 52));
-
-        //System.out.println(numbers.get(new Car("Audi", "A5", 50)));
+        Map<String, Integer> wordMap = new HashMap<>();
+        System.out.println("Please enter some text");
+        Scanner scanner = new Scanner(System.in);
+        String string = scanner.nextLine();
+        String[] tokens = string.split(" ");
+        for (String token : tokens) {
+            String word = token.toLowerCase();
+            Integer count = wordMap.get(word);
+            if (count == null) {
+                wordMap.put(word, 1);
+            } else {
+                wordMap.put(word, count + 1);
+            }
+        }
+        NavigableSet<WordWrapper> wordWrappers = convertToSet(wordMap);
+        printSet(wordWrappers);
 
     }
+
+    private static void printSet(NavigableSet<WordWrapper> wordWrappers) {
+
+    }
+
+    private static NavigableSet<WordWrapper> convertToSet(Map<String, Integer> wordMap) {
+        NavigableSet<WordWrapper> wordSet = new TreeSet<>();
+        for (Map.Entry<String, Integer> e : wordMap.entrySet()) {
+            wordSet.add(new WordWrapper(e.getKey(), e.getValue()));
+        }
+        return wordSet;
+    }
+
+//    private static void printMap(Map<String, Integer> wordMap) {
+//        Map<String, Integer> wordTreeMap = new TreeMap<>(wordMap);
+//        Set<String> keys = wordTreeMap.keySet();
+//        for (String key : keys) {
+//            System.out.printf("%-10s%-10s \n", key, wordMap.get(key));
+//        }
+//    }
 }
