3393d6728682ef11b04c310e65557f363f093686
Vadim-BG <infotourbg.vadim@gmail.com>
2019-10-18 13:38:50 +0300

    Запись в файл с помощью FileWriter класса Part 1

---
+++ src/com/javalesson/javaio/IOMain.java
@@ -0,0 +1,36 @@
+package com.javalesson.javaio;
+
+import com.javalesson.collections.map.treemap.AverageStudentGrade;
+import com.javalesson.collections.map.treemap.SubjectGrade;
+import com.javalesson.collections.map.treemap.TreeMapRunner;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.NavigableMap;
+import java.util.Set;
+
+public class IOMain {
+    public static void main(String[] args) {
+        NavigableMap<AverageStudentGrade, Set<SubjectGrade>> grades = TreeMapRunner.createGrades();
+        FileWriter writer = null;
+        try {
+            writer = new FileWriter("GradeBook.txt");
+            for (AverageStudentGrade gradeKey : grades.keySet()) {
+                writer.write("=========================================\n");
+                writer.write("Students: " + gradeKey.getName() + " Average grade: " + gradeKey.getAverageGrade() + "\n");
+                for (SubjectGrade grade : grades.get(gradeKey)) {
+                    writer.write("Subject: " + grade.getSubject() + " Grade: " + grade.getGrade() + "\n");
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            try {
+                writer.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
